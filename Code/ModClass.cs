3fda60d007026c813258ced30c3f6dd110ab8e15
Corruda <107373242+Corruda@users.noreply.github.com>
2024-08-24 11:55:02 -0400

    Add files via upload

---
+++ Code/ModClass.cs
@@ -0,0 +1,30 @@
+﻿using NeoModLoader.api;
+using UnityEngine;
+using life;
+using AOT;
+using ai;
+using WorldBoxConsole;
+
+
+namespace ModTemplate.Code
+{
+    public class ModClass : BasicMod<ModClass>
+    {
+        protected override void OnModLoad()
+        {
+            // Load your mod here
+
+            WorldBoxConsole.Console.print("Mod has loaded.");
+
+            // Init the traits
+            Traits.init();
+            TraitGroup.init();
+            
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Code/TraitsGroups.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModTemplate.Code
+{
+    class TraitGroup
+    {
+        public static string MoreStuff = "MoreStuff";
+
+        public static void init()
+        {
+            ActorTraitGroupAsset MoreStuff = new ActorTraitGroupAsset();
+            MoreStuff.id = "MoreStuff";
+            MoreStuff.name = "MoreStuff";
+            MoreStuff.color = Toolbox.makeColor("#5b16c9", 1f);
+            AssetManager.trait_groups.add(MoreStuff);
+        }
+    }
+}
